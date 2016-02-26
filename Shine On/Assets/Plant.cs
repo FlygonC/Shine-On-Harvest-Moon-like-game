ef7b1fe5fd47151c8d97d6d61f838e7fd8c804ec
unknown <ryan.floyd@SEA-8E40F2.sea.aie.edu>
2016-02-26 10:09:45 -0800

    More fixes

---
+++ Shine On/Assets/Plant.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "New Plant", menuName = "Plant", order = 2)]
+public class Plant : ScriptableObject
+{
+    public int growTime;// Days per stage of growth
+    public int stages;// Number of Stages of growth
+    public float fragility;// Health lost in harmfull conditions
+    public Mesh[] stagesMeshs;// Visual Meshes for stages
+    public bool dieOnHarvest;// If dies When Harvested
+    public int harvestRevertStage;// Stage to revert to on harvest
+    public ItemObject yield;// Item you get from Harvesting
+}
